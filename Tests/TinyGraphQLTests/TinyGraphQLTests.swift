2e11e4aa27dd425ce5e3841ef8e18e1c6305ed65
cardoso <matheus@cardo.so>
2021-01-04 20:35:46 -0300

    Update tests

--- Tests/TinyGraphQLTests/TinyGraphQLTests.swift
+++ Tests/TinyGraphQLTests/TinyGraphQLTests.swift
@@ -2,14 +2,21 @@ import XCTest
 @testable import TinyGraphQL
 
 final class TinyGraphQLTests: XCTestCase {
-    func testExample() {
+    func testInit() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(TinyGraphQL().text, "Hello, World!")
+        let graphQL = TinyGraphQL(
+            url: URL(string: "https://combase.app/graphql")!,
+            headers: ["combase-organization": "orgid", "Content-Type": "application/json"]
+        )
+        
+        XCTAssertEqual(graphQL.url.absoluteString, "https://combase.app/graphql")
+        XCTAssertEqual(graphQL.headers["combase-organization"], "orgid")
+        XCTAssertEqual(graphQL.headers["Content-Type"], "application/json")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testInit", testInit),
     ]
 }
